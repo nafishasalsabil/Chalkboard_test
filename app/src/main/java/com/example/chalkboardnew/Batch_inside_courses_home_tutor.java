09e9e1ca41d494f13dacc2d700cf18ee820956e9
Dr.Murshid M.Neaz <sadiarahmanpriyota@gmail.com>
2020-09-12 16:20:16 +0600

    pdf files

---
+++ app/src/main/java/com/example/chalkboardnew/Batch_inside_courses_home_tutor.java
@@ -0,0 +1,14 @@
+package com.example.chalkboardnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Batch_inside_courses_home_tutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batch_inside_home_tutor);
+    }
+}
