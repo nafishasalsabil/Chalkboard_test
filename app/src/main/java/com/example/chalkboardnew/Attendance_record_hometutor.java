796a48dc927ea9c8e909387044bc45844dc2a71a
Dr.Murshid M.Neaz <sadiarahmanpriyota@gmail.com>
2020-09-14 00:34:13 +0600

    pdf files

---
+++ app/src/main/java/com/example/chalkboardnew/Attendance_record_hometutor.java
@@ -0,0 +1,14 @@
+package com.example.chalkboardnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Attendance_record_hometutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_record_hometutor);
+    }
+}
---
+++ app/src/main/java/com/example/chalkboardnew/MyNotes_hometutor.java
@@ -0,0 +1,14 @@
+package com.example.chalkboardnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyNotes_hometutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_notes_hometutor);
+    }
+}
