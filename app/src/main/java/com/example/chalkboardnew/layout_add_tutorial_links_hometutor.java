263842fc9a4d80101f06b8ce308dbe364c5d9872
Dr.Murshid M.Neaz <sadiarahmanpriyota@gmail.com>
2020-09-14 02:02:22 +0600

    pdf files

---
+++ app/src/main/java/com/example/chalkboardnew/layout_add_tutorial_links_hometutor.java
@@ -0,0 +1,14 @@
+package com.example.chalkboardnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_add_tutorial_links_hometutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_add_tutorial_links_hometutor);
+    }
+}
