a6cd90bb429fb3fb984c26ad6925362b415a7771
nafishasalsabil <nafishasalsabil16@gmail.com>
2020-09-14 19:50:11 +0600

    important changes.

--- app/src/main/java/com/example/chalkboardnew/ChoiceActivity.java
+++ app/src/main/java/com/example/chalkboardnew/ChoiceActivity.java
@@ -67,7 +67,7 @@ Boolean select = false;
                 DocumentReference documentReference = firestore.collection("users").document(userID);
                 Map<String,Object> user = new HashMap<>();
                 user.put("choice",proff);
-                documentReference.set(user, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                documentReference.update(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         startActivity(new Intent(getApplicationContext(), Features.class));
@@ -113,7 +113,8 @@ Boolean select = false;
                 DocumentReference documentReference = firestore.collection("users").document(userID);
                 Map<String,Object> user = new HashMap<>();
                 user.put("choice",home);
-                documentReference.set(user, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                documentReference.update(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         startActivity(new Intent(getApplicationContext(), MainActivity_HomeTutor.class));
